d05ee29fa24d36baeb1ca99acd4b59f253b0a000
yami2200 <theyami22@gmail.com>
2022-07-12 15:16:02 +0200

    Language server now support Variable import & export diagnostics.

--- src/main/java/PolyglotDiagnosticsHandler.java
+++ src/main/java/PolyglotDiagnosticsHandler.java
@@ -1,8 +1,11 @@
+import com.example.polyglotast.PolyglotTreeHandler;
 import com.google.common.collect.ImmutableList;
+import kotlin.Pair;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.PublishDiagnosticsParams;
 import org.eclipse.lsp4j.services.LanguageClient;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -11,28 +14,28 @@ public class PolyglotDiagnosticsHandler {
 
     private PolyglotLanguageServer languageServer;
 
-    private HashMap<String, HashMap<DiagnosticCategory, HashSet<Diagnostic>>> diagnostics;
+    private HashMap<String, HashMap<Pair<DiagnosticCategory, Path>, HashSet<Diagnostic>>> diagnostics;
 
     public PolyglotDiagnosticsHandler(PolyglotLanguageServer languageServer){
         this.languageServer = languageServer;
-        diagnostics = new HashMap<String, HashMap<DiagnosticCategory, HashSet<Diagnostic>>>();
+        this.diagnostics = new HashMap<>();
     }
 
-    public void addDiagnostic(String uri, Diagnostic diagnostic, DiagnosticCategory category){
+    public void addDiagnostic(String uri, Diagnostic diagnostic, DiagnosticCategory category, Path hostPath){
         if (diagnostics!=null && !category.equals(DiagnosticCategory.ALL)) {
             if(this.diagnostics.containsKey(uri)){
-                if(this.diagnostics.get(uri).containsKey(category)) {
-                    this.diagnostics.get(uri).get(category).add(diagnostic);
+                if(this.diagnostics.get(uri).containsKey(new Pair(category, hostPath))) {
+                    this.diagnostics.get(uri).get(new Pair(category, hostPath)).add(diagnostic);
                 } else {
                     HashSet<Diagnostic> set = new HashSet<Diagnostic>();
                     set.add(diagnostic);
-                    this.diagnostics.get(uri).put(category, set);
+                    this.diagnostics.get(uri).put(new Pair(category, hostPath), set);
                 }
             } else {
-                HashMap<DiagnosticCategory, HashSet<Diagnostic>> map = new HashMap<DiagnosticCategory, HashSet<Diagnostic>>();
+                HashMap<Pair<DiagnosticCategory, Path>, HashSet<Diagnostic>> map = new HashMap<>();
                 HashSet<Diagnostic> set = new HashSet<Diagnostic>();
                 set.add(diagnostic);
-                map.put(category, set);
+                map.put(new Pair(category, hostPath), set);
                 this.diagnostics.put(uri, map);
             }
         }
@@ -58,14 +61,14 @@ public class PolyglotDiagnosticsHandler {
         }
     }
 
-    public void clearDiagnostics(String uri, DiagnosticCategory category){
+    public void clearDiagnostics(String uri, DiagnosticCategory category, Path hostPath){
         LanguageClient client = this.languageServer.languageClient;
         if (client!=null && this.diagnostics!=null && this.diagnostics.containsKey(uri)) {
             if(category.equals(DiagnosticCategory.ALL)){
-                this.clearDiagnostics(uri, DiagnosticCategory.FILENOTFOUND);
-                this.clearDiagnostics(uri, DiagnosticCategory.IMPORTEXPORT);
+                this.clearDiagnostics(uri, DiagnosticCategory.FILENOTFOUND, hostPath);
+                this.clearDiagnostics(uri, DiagnosticCategory.IMPORTEXPORT, hostPath);
             } else {
-                this.diagnostics.get(uri).remove(category);
+                this.diagnostics.get(uri).remove(new Pair(category, hostPath));
             }
         }
     }
--- src/main/java/PolyglotTextDocumentService.java
+++ src/main/java/PolyglotTextDocumentService.java
@@ -129,6 +129,7 @@ public class PolyglotTextDocumentService implements TextDocumentService {
         PolyglotTreeHandler tree_changed = PolyglotTreeHandler.getfilePathToTreeHandler().get(path);
         tree_changed.reparsePolyglotTree(newCode);
 
+        // CHECK ERROR NOT FOUND ERRORS
         HashSet<Path> paths = new HashSet<>();
         paths.add(path);
         for (PolyglotTreeHandler subTree : tree_changed.getSubTrees()) {
@@ -136,8 +137,11 @@ public class PolyglotTextDocumentService implements TextDocumentService {
         }
         this.checkFileNotFound(paths);
 
-        for (Path pathSubTrees : paths) {
-            this.diagHandler.publishDiagnostics(pathSubTrees.toUri().toString());
+        // CHECK IMPORT / EXPORT ERRORS
+        paths.addAll(this.checkInconsistencies(tree_changed.getHostTrees()));
+
+        for (Path pathUpdatedTree : paths) {
+            this.diagHandler.publishDiagnostics(pathUpdatedTree.toUri().toString());
         }
     }
 
@@ -151,8 +155,11 @@ public class PolyglotTextDocumentService implements TextDocumentService {
 
         this.createTreesFromDirectory(path.getParent().toString());
 
+        HashSet<PolyglotTreeHandler> hostTrees = newTree.getHostTrees();
+
+        // CHECK ERROR NOT FOUND ERRORS
         HashSet<Path> paths = new HashSet<>();
-        for (PolyglotTreeHandler hostTree : newTree.getHostTrees()) {
+        for (PolyglotTreeHandler hostTree : hostTrees) {
             if(PolyglotTreeHandler.getfilePathOfTreeHandler().containsKey(hostTree)) paths.add(PolyglotTreeHandler.getfilePathOfTreeHandler().get(hostTree));
             for (PolyglotTreeHandler subTree : hostTree.getSubTrees()) {
                 if(PolyglotTreeHandler.getfilePathOfTreeHandler().containsKey(subTree)) paths.add(PolyglotTreeHandler.getfilePathOfTreeHandler().get(subTree));
@@ -160,8 +167,11 @@ public class PolyglotTextDocumentService implements TextDocumentService {
         }
         this.checkFileNotFound(paths);
 
-        for (Path pathSubTrees : paths) {
-            this.diagHandler.publishDiagnostics(pathSubTrees.toUri().toString());
+        // CHECK IMPORT / EXPORT ERRORS
+        paths.addAll(this.checkInconsistencies(hostTrees));
+
+        for (Path pathUpdatedTree : paths) {
+            this.diagHandler.publishDiagnostics(pathUpdatedTree.toUri().toString());
         }
     }
 
@@ -216,7 +226,7 @@ public class PolyglotTextDocumentService implements TextDocumentService {
         HashMap<Path, HashSet<FileNotFoundInfo>> filesNotFound = new HashMap<>();
 
         for(Path path : paths){
-            this.diagHandler.clearDiagnostics(path.toUri().toString(), DiagnosticCategory.FILENOTFOUND);
+            this.diagHandler.clearDiagnostics(path.toUri().toString(), DiagnosticCategory.FILENOTFOUND, path);
             if(PolyglotTreeHandler.getfilePathToTreeHandler().containsKey(path)){
                 PolyglotTreeHandler tree = PolyglotTreeHandler.getfilePathToTreeHandler().get(path);
                 for(FileNotFoundInfo fileNotFound : tree.getFilesNotFound()){
@@ -238,20 +248,79 @@ public class PolyglotTextDocumentService implements TextDocumentService {
                 diagnostic.setSource("Polyglot EvalFile");
                 diagnostic.setRange(new Range(new Position(file.getLine_position(),file.getChar_position()), new Position(file.getLine_position_end(), file.getChar_position_end())));
                 diagnostic.setSeverity(DiagnosticSeverity.Error);
-                this.diagHandler.addDiagnostic(path.toUri().toString(), diagnostic, DiagnosticCategory.FILENOTFOUND);
+                this.diagHandler.addDiagnostic(path.toUri().toString(), diagnostic, DiagnosticCategory.FILENOTFOUND, path);
             }
         }
     }
 
-    public void checkInconsistencies(PolyglotTreeHandler hostTree){
-        PolyglotDUBuilder du = new PolyglotDUBuilder();
-        hostTree.apply(du);
-        for (Path path : du.getPathsCovered()) {
-            this.diagHandler.clearDiagnostics(path.toUri().toString(), DiagnosticCategory.IMPORTEXPORT);
-        }
-        for (ExportImportStep exportImportStep : du.getListOperation()) {
+    public HashSet<Path> checkInconsistencies(HashSet<PolyglotTreeHandler> trees){
+        HashSet<Path> pathsCovered = new HashSet<>();
+        HashSet<String> diagnosticsID = new HashSet<>();
+        for (PolyglotTreeHandler tree : trees) {
+            PolyglotDUBuilder du = new PolyglotDUBuilder();
+            tree.apply(du);
+
+            for (Path path : du.getPathsCovered()) {
+                this.diagHandler.clearDiagnostics(path.toUri().toString(), DiagnosticCategory.IMPORTEXPORT, PolyglotTreeHandler.getfilePathOfTreeHandler().get(tree));
+                if(!pathsCovered.contains(path)){
+                    pathsCovered.add(path);
+                    this.diagHandler.clearDiagnostics(path.toUri().toString(), DiagnosticCategory.IMPORTEXPORT, path);
+                }
+            }
+            for (String s : du.getExportWithoutImport().keySet()) {
+                for (ExportData exportData : du.getExportWithoutImport().get(s)) {
+                    if(!diagnosticsID.contains(exportData.getId())){
+                        diagnosticsID.add(exportData.getId());
+                        this.addImportExportDiagnostic("Variable \""+ exportData.getVar_name()+"\" is exported but never imported.",
+                                "Polyglot Unused Export", DiagnosticSeverity.Information, exportData.getFilePath(),
+                                new Range(new Position(exportData.getLine_pos(), exportData.getChar_pos()), new Position(exportData.getLine_pos_end(), exportData.getChar_pos_end())),
+                                exportData.getFilePath());
+                    }
+                }
+            }
+
+            for (String s : du.getImportBeforeExport().keySet()) {
+                for (ImportData importData : du.getImportBeforeExport().get(s)) {
+                    this.addImportExportDiagnostic("Variable \""+ importData.getVar_name()+"\" is imported before the export statement. (With Host : "+PolyglotTreeHandler.getfilePathOfTreeHandler().get(tree).getFileName().toString()+")",
+                            "Polyglot Import Order Warning", DiagnosticSeverity.Warning, importData.getFilePath(),
+                            new Range(new Position(importData.getLine_pos(), importData.getChar_pos()), new Position(importData.getLine_pos_end(), importData.getChar_pos_end())),
+                            PolyglotTreeHandler.getfilePathOfTreeHandler().get(tree));
+                }
+            }
+
+            for (String s : du.getImportWithoutExport().keySet()) {
+                for (ImportData importData : du.getImportWithoutExport().get(s)) {
+                    this.addImportExportDiagnostic("Variable \""+ importData.getVar_name()+"\" is imported but is never exported. (With Host : "+PolyglotTreeHandler.getfilePathOfTreeHandler().get(tree).getFileName().toString()+")",
+                            "Polyglot Undefined Import", DiagnosticSeverity.Warning, importData.getFilePath(),
+                            new Range(new Position(importData.getLine_pos(), importData.getChar_pos()), new Position(importData.getLine_pos_end(), importData.getChar_pos_end())),
+                            PolyglotTreeHandler.getfilePathOfTreeHandler().get(tree));
+                }
+            }
+
+            for (String s : du.getImportFromSameFile().keySet()) {
+                for (ImportData importData : du.getImportFromSameFile().get(s)) {
+                    if(!diagnosticsID.contains(importData.getId())) {
+                        diagnosticsID.add(importData.getId());
+                        this.addImportExportDiagnostic("Useless Variable Import. Variable \"" + importData.getVar_name() + "\" is imported from the same file it was exported.",
+                                "Polyglot Useless Import", DiagnosticSeverity.Information, importData.getFilePath(),
+                                new Range(new Position(importData.getLine_pos(), importData.getChar_pos()), new Position(importData.getLine_pos_end(), importData.getChar_pos_end())),
+                                importData.getFilePath());
+                    }
+                }
+            }
+
 
         }
+        return pathsCovered;
+    }
+
+    private void addImportExportDiagnostic(String message, String source, DiagnosticSeverity severity, Path path, Range range, Path diagOwner){
+        Diagnostic diagnostic = new Diagnostic();
+        diagnostic.setMessage(message);
+        diagnostic.setSource(source);
+        diagnostic.setRange(range);
+        diagnostic.setSeverity(severity);
+        this.diagHandler.addDiagnostic(path.toUri().toString(), diagnostic, DiagnosticCategory.IMPORTEXPORT, diagOwner);
     }
 
     /*@Override
@@ -271,57 +340,4 @@ public class PolyglotTextDocumentService implements TextDocumentService {
         });
     }*/
 
-    /*public void checkInconsistencies(String uri){
-        PolyglotDUBuilder Du = new PolyglotDUBuilder();
-        appPolyglotTree.apply(Du);
-        HashMap<String, LinkedList<kotlin.Pair<Integer, Integer>>> map = Du.getImportInconsistencies();
-        HashSet<Diagnostic> listDiag = new HashSet<>();
-        for(String s : map.keySet()){
-            for(kotlin.Pair<Integer, Integer> p : map.get(s)){
-                Diagnostic diagnostic = new Diagnostic();
-                diagnostic.setMessage(s+" : variable was imported but never exported");
-                diagnostic.setSource("Polyglot");
-                diagnostic.setRange(new Range(new Position(p.component1(),0), new Position(p.component1(), Integer.MAX_VALUE)));
-                diagnostic.setSeverity(DiagnosticSeverity.Warning);
-                listDiag.add(diagnostic);
-            }
-        }
-
-        HashMap<String, kotlin.Pair<Integer, Integer>> map_exp = Du.getExportInconsistencies();
-        for(String s : map_exp.keySet()){
-            kotlin.Pair<Integer, Integer> p = map_exp.get(s);
-            Diagnostic diagnostic = new Diagnostic();
-            diagnostic.setMessage(s+" : variable was exported but never imported");
-            diagnostic.setSource("Polyglot");
-            diagnostic.setRange(new Range(new Position(p.component1(),0), new Position(p.component1(), Integer.MAX_VALUE)));
-            diagnostic.setSeverity(DiagnosticSeverity.Information);
-            listDiag.add(diagnostic);
-        }
-
-        this.diagHandler.addDiagnostics(uri, listDiag);
-    }*/
-
-
-    /*
-    public void createTree(String uri) throws URISyntaxException, IOException {
-        PolyglotFileInfo fileInfo = new PolyglotFileInfo(uri);
-        if(fileInfo.isHost()){
-            this.clientLogger.logMessage("Host File opened");
-            if(!fileInfo.getLanguage().equals("none")){
-                try {
-                    this.diagHandler.clearDiagnostics();
-                    //PolyglotTreeHandler.resetFilePathOfTrees();
-
-                    appPolyglotTree = new PolyglotTreeHandler(Paths.get(new URI(fileInfo.getUri())), fileInfo.getLanguage());
-
-                    this.checkFileNotFound();
-                    this.checkInconsistencies(fileInfo.getUri());
-
-                    this.diagHandler.publishDiagnostics();
-                } catch (Exception e) {
-                    System.err.println("Error : "+e.getMessage());
-                }
-            }
-        }
-    }*/
 }
